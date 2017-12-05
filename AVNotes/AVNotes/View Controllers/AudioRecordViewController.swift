7d037bb2a740d7eecb358f63ab3d41e1d564ebc9
KevinTMiller <32279052+KevinTMiller@users.noreply.github.com>
2017-12-05 13:04:14 -0500

    Added more scenes to the storyboard

---
+++ AVNotes/AVNotes/View Controllers/AudioRecordViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AudioRecordViewController.swift
+//  AVNotes
+//
+//  Created by Kevin Miller on 12/5/17.
+//  Copyright © 2017 Kevin Miller. All rights reserved.
+//
+
+import UIKit
+
+class AudioRecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
