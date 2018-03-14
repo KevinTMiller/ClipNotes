b588d1cf18a02fb827c3a8f75f7f0d9085d66f4a
Kevin Miller <32279052+KevinTMiller@users.noreply.github.com>
2018-03-14 10:45:06 -0400

    brigtened up app icon (#29)

--- AVNotes/AVNotes/AppDelegate.swift
+++ AVNotes/AVNotes/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool { // swiftlint:disable:this line_length
         RecordingManager.sharedInstance.loadFiles()
         application.statusBarStyle = .lightContent
         FirebaseApp.configure()
