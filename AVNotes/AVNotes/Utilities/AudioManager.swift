41d024142c61ee4b9c8d34510181044211fceb77
Kevin Miller <32279052+KevinTMiller@users.noreply.github.com>
2018-03-27 15:44:21 -0400

    Fixed bug when changing recording name or deleting current recording (#40)

--- AVNotes/AVNotes/Utilities/AudioManager.swift
+++ AVNotes/AVNotes/Utilities/AudioManager.swift
@@ -176,7 +176,7 @@ StateManagerModelDelegate {
     // then creating a instance of the AVAudioRecord with the current path and settings. */
 
     @objc
-    func handleInterruption(_ notification: Notification) {
+    func handleInterruption(_ notification: Notification) { // swiftlint:disable:this cyclomatic_complexity
         guard let info = notification.userInfo,
             let typeValue = info[AVAudioSessionInterruptionTypeKey] as? UInt,
             let type = AVAudioSessionInterruptionType(rawValue: typeValue) else {
@@ -316,7 +316,7 @@ StateManagerModelDelegate {
         stateManager.currentState = .prepareToPlay
     }
 
-    private func setBlankRecording() {
+    func setBlankRecording() {
         currentRecording = createAnnotatedRecording()
     }
 
